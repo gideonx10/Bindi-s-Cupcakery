364d5de9f02ddd81846a76e736f4cca23cbe799e
Nirmeet <nirmeetparmar21@gmail.com>
2025-02-13 00:13:48 +0530

    idk

--- src/app/Homepage/page.tsx
+++ src/app/Homepage/page.tsx
@@ -6,13 +6,8 @@ import FlowMenu from "./pages/FlowMenu";
 import TopCategories from "./pages/TopCategories";
 import ImageGallery from "./pages/ImageFallery";
 // import BlogsSection from "./pages/BlogsSection";
-<<<<<<< HEAD
-export default function Page(){
-  
-=======
 
 export default function Page() {
->>>>>>> 81ee3805c10b7d16db6577ab788f3d881e513e02
   return (
     <div>
       {/* <Navbar /> */}
