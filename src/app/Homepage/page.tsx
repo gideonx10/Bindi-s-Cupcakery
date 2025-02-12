43a352b9ab494a84029f50fa140358cc52aa7d80
Nirmeet <nirmeetparmar21@gmail.com>
2025-02-12 18:48:19 +0530

    Top categories page done

--- src/app/Homepage/page.tsx
+++ src/app/Homepage/page.tsx
@@ -1,7 +1,7 @@
 import Landing from "./pages/Landing";
 import { InfiniteName } from "./pages/InfiniteName";
 import FlowMenu from "./pages/FlowMenu";
-
+import TopCategories from "./pages/TopCategories";
 
 export default function Page(){
   
@@ -10,6 +10,7 @@ export default function Page(){
       <Landing />
       <InfiniteName />
       <FlowMenu />
+      <TopCategories />
     </div>
   );
 }
