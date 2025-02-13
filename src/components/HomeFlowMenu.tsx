fae2f23fc1867e9ae046db07cfb19cf4d712dbbb
Nirmeet <nirmeetparmar21@gmail.com>
2025-02-13 22:34:24 +0530

    Made homepage app-router based

--- src/components/HomeFlowMenu.tsx
+++ src/components/HomeFlowMenu.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React from 'react';
-import FlowingMenu from "../Effects/FlowingMenu";
+import FlowingMenu from "../app/Homepage/Effects/FlowingMenu";
 
 const FlowMenu = () => {
   const demoItems = [
--- src/components/HomeInfiniteName.tsx
+++ src/components/HomeInfiniteName.tsx
@@ -1,4 +1,4 @@
-import InfiniteText from "../Effects/InfiniteText";
+import InfiniteText from "../app/Homepage/Effects/InfiniteText";
 
 export function InfiniteName() {
   return (
