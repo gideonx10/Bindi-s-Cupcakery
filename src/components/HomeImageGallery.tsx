e1c90dd752ef72a82aede3bc66c58aa5e88b5234
gideonx10 <adityapanchal271004@gmail.com>
2025-02-15 00:53:15 +0530

    fixed auth route - 3

--- src/components/HomeImageGallery.tsx
+++ src/components/HomeImageGallery.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import Image from 'next/image';
 import { motion, useMotionValue } from 'framer-motion';
 
